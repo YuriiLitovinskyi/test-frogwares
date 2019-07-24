4bcd79178442585f764e1904aa00ce6347a1a09c
YuriiLitovinskyi <y.litovinskyi@gmail.com>
2019-07-24 23:54:23 +0300

    starting vue client, displaying first data + cors rules on server side

---
+++ client/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-bind:key="data.id" v-for="data in mainPageInfo">
+      <h3> {{data.alias}} </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+
+  data(){
+  return {
+    mainPageInfo: []
+  }
+},
+
+methods: {
+ 
+},
+
+created(){ 
+    axios.get("http://localhost:3004/")
+    .then(res => this.mainPageInfo = res.data)
+    .catch(err => console.log(err));
+  } 
+}
+
+
+
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
